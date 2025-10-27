38a316d54df0365107509915a573a1d458dae1cf
soroushiliyat <soroushiliyat23@gmail.com>
2025-10-27 21:46:57 +0330

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
-// src/App.tsx
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-import LoginForm from "./components/LoginForm";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 import SignupForm from "./components/SignupForm";
-import Dashboard from "./pages/Dashboard";
-import { useAuthStore } from "./store/authStore";
+import LoginForm from "./components/LoginForm";
 
-function App() {
-  const token = useAuthStore((state) => state.token);
-
+export default function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Navigate to={token ? "/dashboard" : "/login"} />} />
-        <Route path="/login" element={<LoginForm />} />
-        <Route path="/signup" element={<SignupForm />} />
-        <Route
-          path="/dashboard"
-          element={token ? <Dashboard /> : <Navigate to="/login" />}
-        />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/signup" element={<SignupForm />} />
+      <Route path="/login" element={<LoginForm />} />
+    </Routes>
   );
 }
-
-export default App;
